6c45c24aacc4c323c07693375d55bc58633b4219
VagyokC4 <vagyok.c4@gmail.com>
2020-05-02 13:12:52 -0700

    remove dependency on build output folder and back to nuget package folder for redis binaries

--- src/Redis2Go/RedisTypes.cs
+++ src/Redis2Go/RedisTypes.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.IO;
+using System.Linq;
 
 namespace Redis2Go
 {
@@ -11,17 +12,19 @@ namespace Redis2Go
 
     public static class RedisTypesExtensions
     {
-        private static readonly string CurrentDirectory = AppDomain.CurrentDomain.BaseDirectory;
-        private static readonly string Redis64 = $@"{CurrentDirectory}\tools\Redis64\";
-        private static readonly string Redis32 = $@"{CurrentDirectory}\tools\Redis32\";
+        private static readonly string CurrentDirectory = new DirectoryInfo(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), @".nuget\packages\redis2go.alpha\"))
+            .GetFileSystemInfos("tools", SearchOption.AllDirectories).OrderByDescending(x => x.LastWriteTimeUtc).First().FullName;
 
-        public static string GetBinaryPath(this RedisTypes @this)
+        private static readonly string Redis64 = $@"{CurrentDirectory}\Redis64\";
+        private static readonly string Redis32 = $@"{CurrentDirectory}\Redis32\";
+
+        public static string GetBinaryPath( this RedisTypes @this )
         {
-            switch (@this)
+            switch ( @this )
             {
-                case RedisTypes.Redis64:return Redis64;
-                case RedisTypes.Redis32:return Redis32;
-                default:throw new ArgumentOutOfRangeException();
+                case RedisTypes.Redis64: return Redis64;
+                case RedisTypes.Redis32: return Redis32;
+                default:                 throw new ArgumentOutOfRangeException();
             }
         }
     }
